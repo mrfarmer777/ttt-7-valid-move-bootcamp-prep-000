828557159e7adb7cce052c535047797d288cfe4a
Matt Farmer <mrfarmer777@gmail.com>
2018-03-28 04:29:46 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,8 @@
 # code your #valid_move? method here
-
+def valid_move?(board,ind)
+  if ind < 9 
+  if !(position_taken(board,ind))
+    
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,ind)
